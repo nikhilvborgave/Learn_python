4cc177c58d8a1489533891abf081cebf293f11a3
Nikhil_Borgave <nikhilvborgave30@gmail.com>
2025-02-27 00:44:54 +0530

    code to check leap year

--- Day-10_pause_challenges.py
+++ Day-10_pause_challenges.py
@@ -11,3 +11,34 @@ def format_name(f_name, l_name):
 format_name(f_name="NIKHIL",l_name="BOrgAVE")
 
 
+## CHECK LEAP YEAR
+
+# def format_name(f_name, l_name):
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+#     return f"{formated_f_name} {formated_l_name}"
+#
+#
+# print(format_name("AnGEla", "YU"))
+
+
+def is_leap_year(year):
+    # Write your code here.
+    # Don't change the function name.
+    leap_year = False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap_year = True
+            else:
+                leap_year = False
+        else:
+            leap_year = True
+    else:
+        leap_year = False
+    return(leap_year)
+    print(leap_year)
+
+
+leap_year = is_leap_year(2021)
+print(leap_year)
