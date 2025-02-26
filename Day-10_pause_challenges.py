314bf3b3ac75499c1cb2fd898fb2094ab5f3d9ce
Nikhil_Borgave <nikhilvborgave30@gmail.com>
2025-02-26 10:04:03 +0530

    pause challenge 1

---
+++ Day-10_pause_challenges.py
@@ -0,0 +1,13 @@
+def format_name(f_name, l_name):
+    first_name = f_name.title()
+    last_name = l_name.title()
+    print(first_name, last_name)
+
+    #OR we can use following one line to get the same output
+
+    print(f_name.title(), l_name.title())
+
+
+format_name(f_name="NIKHIL",l_name="BOrgAVE")
+
+
