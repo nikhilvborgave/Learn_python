295197cf228a4bdf79d0395664c7547df390f9ea
Nikhil_Borgave <nikhilvborgave30@gmail.com>
2025-03-03 17:02:13 +0530

    Added updated code to get random number from 1-100 & added functions

--- Day_12_number_guessing/Day-12_number_guessing_game.py
+++ Day_12_number_guessing/Day-12_number_guessing_game.py
@@ -1,32 +1,36 @@
 import random
-numbers = []
-for i in range(1, 101):
-    numbers.append(i)
+LIVES = ""
 
-chosen_number = random.choice(numbers)
+def lives():
+    difficulty = input("Choose difficulty. Type 'easy' or 'hard': ").lower()
+    global LIVES
+    if difficulty == "easy":
+        LIVES = 10
+    elif difficulty == "hard":
+        LIVES = 5
+    print(f"You have {LIVES} attempts remaining to guess the number.")
+
+def game():
+    global LIVES
+    while LIVES != 0:
+        guess = int(input("Make a guess: "))
+
+        if chosen_number > guess:
+            print("Too low")
+            LIVES -= 1
+        elif chosen_number < guess:
+            print("Too high")
+            LIVES -= 1
+        elif chosen_number == guess:
+            print("Correct. You Won!!")
+            break
+    if LIVES == 0:
+        print(f"You lose. Correct number was {chosen_number}")
+
+
+chosen_number = random.randint(1, 100)
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100")
-difficulty = input("Choose difficulty. Type 'easy' or 'hard': ").lower()
-Lives = ""
-if difficulty == "easy":
-    Lives = 10
-elif difficulty == "hard":
-    Lives = 5
-
-print(f"You have {Lives} attempts remaining to guess the number.")
-
-while Lives != 0:
-    guess = int(input("Make a guess: "))
-
-    if chosen_number > guess:
-        print("Too low")
-        Lives -= 1
-    elif chosen_number < guess:
-        print("Too high")
-        Lives -= 1
-    elif chosen_number == guess:
-        print("Correct. You Won!!")
-        break
-if Lives == 0:
-    print(f"You lose. Correct number was {chosen_number}")
+lives()
+game()
