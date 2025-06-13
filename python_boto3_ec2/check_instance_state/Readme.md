a3268ecd63d8230f249c0150c2a1746a293df1f0
Nikhil_Borgave <nikhilvborgave30@gmail.com>
2025-06-13 23:05:49 +0530

    updated script for ip & export csv

--- python_boto3_ec2/check_instance_state/Readme.md
+++
@@ -1,9 +0,0 @@
-<pre>
-python_boto3_ec2/
-└── check_instance_state/
-    ├── instance_state.py
-    └── readme.md
-</pre>
-
-instance_state.py- <br>
-will check the instance state and give output. <br>
--- python_boto3_ec2/check_instance_state/instance_state.py
+++ python_boto3_ec2/check_instance_state/instance_state.py
@@ -1,19 +1,51 @@
 import boto3
+import pandas as pd
 
 client = boto3.client('ec2', region_name='us-east-1')
 
-response = client.describe_instance_status(
+def describe_instance():
+    response = client.describe_instance_status(
+        InstanceIds=[],
+        DryRun= False,      #checks if you have valid permissions
+        IncludeAllInstances=True
+    )
+
+    for instance in response.get("InstanceStatuses", []):
+        instance_id = instance.get("InstanceId", "N/A")
+        state = instance.get("InstanceState", {}).get("Name", "Unknown")
+        ec2_id = instance_id
+        Instance_status = state
+        return ec2_id, Instance_status     #returns values of instance id & status
+
+
+table = []
+describe_ec2 = client.describe_instances(
     InstanceIds=[],
     DryRun= False,      #checks if you have valid permissions
-    IncludeAllInstances=True
 )
 
-for instance in response.get("InstanceStatuses", []):
-    instance_id = instance["InstanceId"]
-    state = instance["InstanceState"]["Name"]
-    print(f"instance_id = {instance_id}")
-    print(f"status = {state}")
+for reservation in describe_ec2.get("Reservations", []):
+    for instance in reservation.get("Instances", []):
+         # Extract Name tag if available
+        name = None
+        for tag in instance.get("Tags", []):
+            if tag["Key"] == "Name":
+                name = tag["Value"]
+        instance_public_ip = instance.get("PublicIpAddress", "N/A")
+        instance_private_ip = instance.get("PrivateIpAddress", "N/A")
+        #calls describe_instance function and stores values resp.
+        ec2_id, Instance_status = describe_instance()  
+        table.append({
+            "Instance Id ": instance.get("InstanceId"),
+            "Instance Name": name,
+            "Public Ip": instance_public_ip,
+            "Private Ip": instance_private_ip,
+            "Status": Instance_status,
+        })
 
+# Create DataFrame
+df = pd.DataFrame(table)
 
-# print("Instance details: ")
-# print(f"- {response} ")        #prints bucket name with date of creation
+# Save to CSV
+df.to_csv("instance_state.csv", index=False)
+print("Complete!!")
